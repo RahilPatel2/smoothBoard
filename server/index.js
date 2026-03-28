cf9721df312466915a7bf26e67c2ee619e2061dd
Rahil Pambhar <149613955+RahilPatel2@users.noreply.github.com>
2026-03-29 01:57:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -210,7 +210,7 @@ app.get("/", (req, res) => {
 });
 
 // 🚀 START SERVER
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`🔥 Server running on port ${PORT}`);
 });
